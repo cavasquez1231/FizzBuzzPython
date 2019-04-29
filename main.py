a814fa491a59d49af1e398cee5970e382e8c5cf6
cavasquez1231 <cavasquez@live.com>
2019-04-28 17:09:07 -0700

    Some FizzBuzz BS

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+
+This is some fizzbuzz shit
+
+"""
+
+def FizzBuzz(number):
+    for num in range(1,number):
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
